9f80f0a0a3266934d4c6a9493918982d3ac4f227
camilog117 <c.gomezcreativo@gmail.com>
2026-03-08 12:56:12 -0500

    Add files via upload

---
+++ userScript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Mercado Play en Tizen TV
+// @namespace    http://tu-nombre/
+// @version      1.0
+// @description  Abre Mercado Play sin complicaciones
+// @author       Camilo
+// @match        https://play.mercadolibre.co/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Mercado Play cargado vía TizenBrew - ¡Disfruta!');
+    // Aquí podrías poner mejoras después si quieres (fullscreen, controles remotos, etc.)
+})();
